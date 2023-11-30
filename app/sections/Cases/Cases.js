fc01561539c9fddd110ab1f1ebbfb7fdbf06d375
Lika <114234069+Basilika-draconum@users.noreply.github.com>
2023-11-30 11:35:28 +0200

    install swiper and create CaseSlider

--- app/sections/Cases/Cases.js
+++ app/sections/Cases/Cases.js
@@ -1,9 +1,14 @@
+import CaseSlider from "@/components/CaseSlider/CaseSlider";
+import Title from "@/components/Title/Title";
 import React from "react";
 
 const Cases = () => {
   return (
-    <section id="cases">
-      <div className="container-main">Cases</div>
+    <section id="cases" className="section-main">
+      <div className="container-main">
+        <Title text={"Successful cases of our company"} className={"mb-24"} />
+        <CaseSlider />
+      </div>
     </section>
   );
 };
---
+++ components/CaseSlider/CaseSlider.js
@@ -0,0 +1,59 @@
+"use client";
+import React from "react";
+import { Swiper, SwiperSlide, SwiperRef } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css/navigation";
+
+import "swiper/css";
+import slides from "@/public/content/casesSlider.json";
+import Image from "next/image";
+
+const carouselSettings = {
+  spaceBetween: 50,
+  modules: [Navigation],
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  grabCursor: true,
+  loop: true,
+  slidesPerView: 1,
+  onSlideChange: () => console.log("slide change"),
+};
+
+const CaseSlider = () => {
+  return (
+    <Swiper {...carouselSettings}>
+      {slides &&
+        slides.map(({ id, photo, title, city, subtitle, date }, index) => (
+          <SwiperSlide key={id} className="">
+            <div className="bg-bgForm">
+              <Image
+                src={photo}
+                alt={title}
+                width={320}
+                height={224}
+                loading="lazy"
+                sizes="(max-width: 767px) 360px, (max-width: 1279px) 400px, 600px"
+              />
+              <div className="p-3 pt-4">
+                <div className="flex gap-[61px]">
+                  <div className="text-question">
+                    <p>{city}</p>
+                    <p className="w-[175px]">{title}</p>
+                  </div>
+                  <button>Button</button>
+                </div>
+                <div className="flex justify-between border-t-[1px] border-accentColor pt-3 text-xs tracking-[-0.48px]">
+                  <p>{subtitle}</p>
+                  <p>{date}</p>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+    </Swiper>
+  );
+};
+
+export default CaseSlider;
