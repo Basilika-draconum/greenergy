6bdd86dc2cbf22620b21202f80903b5c6278ccc7
Lika <114234069+Basilika-draconum@users.noreply.github.com>
2023-12-04 10:07:42 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -32,11 +32,6 @@ export const metadata = {
   },
 };
 
-export const viewport = {
-  width: "device-width",
-  initialScale: 1,
-  maximumScale: 1,
-};
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
